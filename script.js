a6149fd6afe11ee108263ddc2974a12d91da52c1
darbata <batarina.darren@gmail.com>
2024-11-18 10:31:17 +1100

    Add UI to rps game

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const humanScoreUI = document.querySelector(".human-score");
+const computerScoreUI = document.querySelector(".computer-score");
+const log = document.querySelector(".log")
+
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
 	let num = Math.random();
 	let choice;
@@ -18,31 +25,50 @@ let getHumanChoice = () => {
 	return choice = prompt("Choose: Rock, Paper or Scissors");
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 let playRound = (humanChoice, computerChoice) => {
-	humanChoice = humanChoice.trim().toLowerCase();
-
-	if (humanChoice === "rock" && computerChoice === "scissors") {
-		console.log("Congratulations you won")
-		humanScore++
-	} else if (humanChoice === "paper" && computerChoice === "rock") {
-		console.log("Congratulations you won")
-		humanScore++
-	} else if (humanChoice === "scissors" && computerChoice === "paper") {
+	if
+		((humanChoice === "rock" && computerChoice === "scissors") ||
+		(humanChoice === "paper" && computerChoice === "rock") ||
+		(humanChoice === "scissors" && computerChoice === "paper")) {
 		console.log("Congratulations you won")
 		humanScore++
 	} else if (humanChoice === computerChoice) {
 		console.log("It's a draw")
+		log.textContent = "It's a draw";
 	} else {
 		console.log("The computer wins")
 		computerScore++
+		log.textContent = "The computer wins";
+	}
+
+	humanScoreUI.textContent = `Human: ${humanScore}`;
+	computerScoreUI.textContent = `Computer: ${computerScore}`;
+
+	if (humanScore >= 5) {
+		humanScore = 0;
+		computerScore = 0;
+		humanScoreUI.textContent = `Human: ${humanScore}`;
+		computerScoreUI.textContent = `Computer: ${computerScore}`;
+		log.textContent = "Choose: Rock, Paper or Scissors";
+		alert("You win!")
+	}
+	if (computerScore >= 5) {
+		computerScore = 0;
+		humanScore = 0;
+		humanScoreUI.textContent = `Human: ${humanScore}`;
+		computerScoreUI.textContent = `Computer: ${computerScore}`;
+		log.textContent = "Choose: Rock, Paper or Scissors";
+		alert("The computer wins!")
 	}
 }
 
-for (let i = 0; i < 5; i++) {
-	playRound(getHumanChoice(), getComputerChoice());
-}
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
-console.log(`Human Score: ${humanScore}, ComputerScore: ${computerScore}`)
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
+
+
